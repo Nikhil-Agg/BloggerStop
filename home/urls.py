dabcbf1f2959cae24b84416fba9edc65140fdb27
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-11-29 03:13:34 +0530

    classbased and listbased views

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import home, article_detail
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path('', home.as_view(), name='home'),
+    path('post/<int:pk>', article_detail.as_view(), name='detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    context = {}
-    return render(request, 'home.html', context=context)
+
+# def home(request):
+#     context = {}
+#     return render(request, 'home.html', context=context)
+
+class home(ListView):
+
+    model = Post
+    template_name = 'home.html'
+
+class article_detail(DetailView):
+
+    model = Post
+    template_name = 'detail.html'

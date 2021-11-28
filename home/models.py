cfd4ad921a84da99ad3fb51d683bf5a3711ee62f
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-11-29 02:17:12 +0530

    adding model to home

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_no = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title

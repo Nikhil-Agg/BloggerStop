0617aec3c0a658a4a9dfaf14ce0923ea589b63a9
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-12-06 00:20:22 +0530

    created home fodler in templates

--- home/views.py
+++ home/views.py
@@ -6,24 +6,24 @@ from .forms import *
 
 class home(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'home/home.html'
     ordering = ['-created_no']
 
 class article_detail(DetailView):
     model = Post
-    template_name = 'detail.html'
+    template_name = 'home/detail.html'
 
 class article_add(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'add.html'
+    template_name = 'home/add.html'
 
 class article_update(UpdateView):
     model = Post
     form_class = EditForm
-    template_name = 'edit.html'
+    template_name = 'home/edit.html'
 
 class article_delete(DeleteView):
     model = Post
-    template_name = 'delete.html'
+    template_name = 'home/delete.html'
     success_url = reverse_lazy('home')

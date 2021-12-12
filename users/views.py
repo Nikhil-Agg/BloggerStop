600f6b847b51f9e5f11e88c300985674535ffe22
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-12-12 23:13:31 +0530

    user authentication

--- users/views.py
+++ users/views.py
@@ -4,5 +4,5 @@ from django.urls import reverse_lazy
 
 class SignupView(CreateView):
     form_class = UserCreationForm
-    template_name = 'users/signup.html'
+    template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
